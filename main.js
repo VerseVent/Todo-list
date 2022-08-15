5eacbce075aeac730d8776c66d157a4da0b75ec3
VerseVent <mike.82pronka.27a@gmail.com>
2022-08-15 18:16:34 +0200

    remove logs

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ var app = new Vue({
   },
   methods: {
     addItem() {
-      console.log(1);
       if (this.currentItem.length) {
         this.item = {
           task: this.currentItem,
@@ -20,19 +19,14 @@ var app = new Vue({
       return;
     },
     setDone(id) {
-      console.log(2);
-
       this.todoList[id].isDone = !this.todoList[id].isDone;
     },
     deleteItem(id) {
-      console.log(3);
-
       this.todoList = this.todoList.filter((item, i) => i !== id);
     },
   },
   computed: {
     titleColor() {
-      console.log("?");
       if (this.todoList.filter((item) => item.isDone).length === 0)
         return "red";
       if (
